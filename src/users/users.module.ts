cecfba14fb3dcdac2fc75b020b091ed0b28ceb60
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-06-15 11:34:15 -0300

    feat: typeorm config

---
+++ src/users/users.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+@Module({
+  // aqui eu crio o repo automaticamente listando um array do tipo user da entity
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService]
+})
+export class UsersModule {}
