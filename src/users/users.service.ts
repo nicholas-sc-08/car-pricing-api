2749450592bc4e87cd3159ca011fa51c78774a53
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-06-20 22:10:04 -0300

    crud complete + error handlers

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Repository } from "typeorm";
 import { User } from "./user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
@@ -23,13 +23,20 @@ export class UsersService {
 
     async update(id: number, attrs: Partial<User>) {
         const user = await this.findOne(id);
-        if(!user) {
-            throw new Error("user not found");
+        if (!user) {
+            throw new NotFoundException("user not found");
         }
         Object.assign(user, attrs);
-        
+
         return this.repo.save(user);
     }
 
-    remove() { }
+    async remove(id: number) {
+        const user = await this.findOne(id);
+        if (!user) {
+            throw new NotFoundException("user not found");
+        }
+        
+        return this.repo.remove(user);
+    }
 }
