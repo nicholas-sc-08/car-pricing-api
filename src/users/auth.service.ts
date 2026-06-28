26cf952dc6a202f52db495836a0d325976fb6875
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-06-28 19:15:09 -0300

    feat: cookie session

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { UsersService } from "./users.service";
-import { randomBytes, scrypt as _scrypt, Hash } from "crypto";
+import { randomBytes, scrypt as _scrypt } from "crypto";
 import { promisify } from "util";
 
 const scrypt = promisify(_scrypt);
@@ -38,7 +38,7 @@ export class AuthService {
         if (storedHash != hash.toString('hex')) {
             throw new BadRequestException('bad password');
         }
-        
+
         return user;
     }
 }
