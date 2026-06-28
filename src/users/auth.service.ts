d1786acf99ae7da8d85c44247db31c1244bc3763
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-06-27 21:36:14 -0300

    feat: signin

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,17 +1,17 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { UsersService } from "./users.service";
-import { randomBytes, scrypt as _scrypt } from "crypto";
+import { randomBytes, scrypt as _scrypt, Hash } from "crypto";
 import { promisify } from "util";
 
 const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
+    constructor(private usersService: UsersService) { }
 
     async signup(email: string, password: string) {
         const users = await this.usersService.find(email);
-        if(users.length) {
+        if (users.length) {
             throw new BadRequestException('email in use ');
         }
 
@@ -25,7 +25,20 @@ export class AuthService {
         return user;
     }
 
-    signin(email: string, password: string) {
+    async signin(email: string, password: string) {
+        const [user] = await this.usersService.find(email);
+        if (!user) {
+            throw new NotFoundException('user not found');
+        }
 
+        const [salt, storedHash] = user.password.split('.');
+
+        const hash = await scrypt(password, salt, 32) as Buffer;
+
+        if (storedHash != hash.toString('hex')) {
+            throw new BadRequestException('bad password');
+        }
+        
+        return user;
     }
 }
