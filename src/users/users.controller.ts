332ff6366e82bd4b33293670f073decd7eec6212
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-07-11 18:58:18 -0300

    feat: auth guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException, Session } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException, Session, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -7,6 +7,7 @@ import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -22,6 +23,7 @@ export class UsersController {
     // }
 
     @Get('/whoami')
+    @UseGuards(AuthGuard)
     whoAmI(@CurrentUser() user: User) {
         return user;
     }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,11 +4,19 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   // aqui eu crio o repo automaticamente listando um array do tipo user da entity
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService]
+  providers: [UsersService, AuthService,
+    //aqio eu to declarando que o user interceptor vai ser pra todas os controllers do user, globalizando ele, ao inves de ter que ficar declarando em varios lugares diferentes
+    {
+      provide: APP_INTERCEPTOR, 
+      useClass: CurrentUserInterceptor
+    }
+  ]
 })
 export class UsersModule { }
