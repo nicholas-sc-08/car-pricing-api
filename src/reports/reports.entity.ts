8f9a95086b1a83465d66ebc42f0cf44a9fa7ccfe
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-08-10 10:54:27 -0300

    feat: report entity

--- src/reports/reports.entity.ts
+++ src/reports/reports.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { AfterInsert, AfterRemove, AfterUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Report {
@@ -7,4 +7,37 @@ export class Report {
 
     @Column()
     price!: number;
+
+    @Column()
+    make!: string
+
+    @Column()
+    model!: string;
+
+    @Column()
+    year!: number;
+
+    @Column()
+    lng!: string;
+
+    @Column()
+    lat!: number;
+
+    @Column()
+    mileage!: number;
+
+    @AfterInsert()
+    logInsert() {
+        console.log(`Inserting car with id ${this.id}`);
+    }
+
+    @AfterUpdate()
+    logUpdate() {
+        console.log(`Updating car with id ${this.id}`);
+    }
+
+    @AfterRemove()
+    logRemove() {
+        console.log(`Removing car with id ${this.id}`);
+    }
 }
