9d6454976ce22accf7ad34e1ec8f0805e29d9d58
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-08-10 10:48:12 -0300

    feat: cookie tests

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -19,14 +19,31 @@ describe('Authentication System', () => {
   it('handles a signup request', () => {
     const email = '348y293y489@gmail.com';
     return request(app.getHttpServer())
-        .post('/auth/signup')
-        .send({ email: email, password: '1234567'})
-        .expect(201)
-        .then(res => {
-          const { id, email } = res.body;
-          expect(id).toBeDefined();
-          expect(email).toEqual(email);
-        })
+      .post('/auth/signup')
+      .send({ email: email, password: '1234567' })
+      .expect(201)
+      .then(res => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      })
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'wewerwe@gmail.com';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '1234567' })
+      .expect(201)
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(body.email).toEqual(email);
   });
 
   afterEach(async () => {
